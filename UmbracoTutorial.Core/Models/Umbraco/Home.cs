fe7f4db928e561d396863b5b53d4d5baf72ae830
Carl hope <84421131+carlhope@users.noreply.github.com>
2023-11-23 10:46:16 +0000

    update

---
+++ UmbracoTutorial.Core/Models/Umbraco/Home.cs
@@ -0,0 +1,20 @@
+﻿namespace UmbracoTutorial.Core.UmbracoModels
+{
+    public partial class Home
+    {
+        public string ShortHeroDescription
+        {
+            get
+            {
+                if (string.IsNullOrEmpty(HeroDescription))
+                {
+                    return "";
+                }
+                return $"{HeroDescription.Substring(0, 30)}...";
+            }
+
+
+        }
+    }
+}
+
