89c1fa60bfbb28282537c1a201648030d2a8134f
Carl hope <84421131+carlhope@users.noreply.github.com>
2023-12-02 12:04:21 +0000

    completed section 9

---
+++ Udemy_Umbraco_course/Routing/ProductPageUrlSegmentProvider.cs
@@ -0,0 +1,34 @@
+﻿using Umbraco.Cms.Core.Models;
+using Umbraco.Cms.Core.PublishedCache;
+using Umbraco.Cms.Core.Strings;
+
+namespace Udemy_Umbraco_course.Routing
+{
+	public class ProductPageUrlSegmentProvider : IUrlSegmentProvider
+	{
+		private readonly IUrlSegmentProvider _provider;
+		private readonly IPublishedSnapshotAccessor _publishedSnapshotAccessor;
+
+		private readonly string _SKUAlias;
+
+		public ProductPageUrlSegmentProvider(IShortStringHelper stringHelper, IPublishedSnapshotAccessor publishedSnapshotAccessor)
+		{
+			
+			_provider = new DefaultUrlSegmentProvider(stringHelper);
+			_publishedSnapshotAccessor = publishedSnapshotAccessor;
+			_SKUAlias = UmbracoTutorial.Core.UmbracoModels.Product.GetModelPropertyType(_publishedSnapshotAccessor ,x => x.Sku).Alias;
+		}
+		public string? GetUrlSegment(IContentBase content, string? culture = null)
+		{
+			if (content.ContentType.Alias != UmbracoTutorial.Core.UmbracoModels.Product.ModelTypeAlias)
+			{
+				return null;
+			}
+
+			var currentSegment = _provider.GetUrlSegment(content, culture);
+			var productSku = content.GetValue<string>(_SKUAlias).ToLower()??string.Empty;
+
+			return !string.IsNullOrEmpty(productSku) ? $"{currentSegment}-{productSku}" : currentSegment;
+		}
+	}
+}
