aefa519d9e4f38100d4c1991be7c22c7f6e7d69b
Carl hope <84421131+carlhope@users.noreply.github.com>
2023-12-09 13:47:22 +0000

    completed section 5

---
+++ Udemy_Umbraco_course/App_Plugins/BlockListElements/ProductListing/productlisting.controller.js
@@ -0,0 +1,29 @@
+﻿(function () {
+    "use strict";
+
+    function productListingController($scope, productListingService) {
+        var vm = this;
+
+        vm.title = $scope.block.data.title;
+        vm.numberOfProducts = $scope.block?.settingsData?.numberOfProducts;
+
+        vm.products = [];
+
+        getAllProducts();
+
+        $scope.$watch('block.data', function () {
+            vm.title = $scope.block.data.title;
+        }, true);
+        $scope.$watch('block.settingsData', function () {
+            vm.numberOfProducts = $scope.block?.settingsData?.numberOfProducts;
+        }, true);
+
+        function getAllProducts() {
+            productListingService.getProduct(vm.numberOfProducts).then(function (response) {
+                vm.products = response;
+            }, function (error) { console.log(error); });
+        }
+    }
+
+    angular.module("umbraco").controller("productListingController", productListingController);
+})();
