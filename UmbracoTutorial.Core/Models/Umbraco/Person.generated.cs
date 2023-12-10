51d673628afe939a70c452b8aef1aa2d921c1185
Carl hope <84421131+carlhope@users.noreply.github.com>
2023-12-10 14:08:01 +0000

    update

--- UmbracoTutorial.Core/Models/Umbraco/Person.generated.cs
+++ UmbracoTutorial.Core/Models/Umbraco/Person.generated.cs
@@ -74,6 +74,14 @@ namespace UmbracoTutorial.Core.UmbracoModels
 		public virtual string FacebookUsername => this.Value<string>(_publishedValueFallback, "facebookUsername");
 
 		///<summary>
+		/// Github user
+		///</summary>
+		[global::System.CodeDom.Compiler.GeneratedCodeAttribute("Umbraco.ModelsBuilder.Embedded", "12.3.3+d2ff2ea")]
+		[global::System.Diagnostics.CodeAnalysis.MaybeNull]
+		[ImplementPropertyType("githubUser")]
+		public virtual global::Newtonsoft.Json.Linq.JToken GithubUser => this.Value<global::Newtonsoft.Json.Linq.JToken>(_publishedValueFallback, "githubUser");
+
+		///<summary>
 		/// Instagram Username
 		///</summary>
 		[global::System.CodeDom.Compiler.GeneratedCodeAttribute("Umbraco.ModelsBuilder.Embedded", "12.3.3+d2ff2ea")]
